af8c4a19400a0f24044a8f44e232a873646e51fa
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2018-08-11 16:33:06 -0500

    There a Jenkins is Reaaly needed

--- src/main/java/com/unrealdinnerbone/yaum/libs/Reference.java
+++ src/main/java/com/unrealdinnerbone/yaum/libs/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "yaum";
     public static final String MOD_NAME = "Yet Another Util Mod";
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "5.0.28";
     public static final String MC_VERSION = "1.12.2";
     public static final String GUI_FACTORY_CLASS = "com.unrealdinnerbone.yaum.client.gui.ModConfigGUI";
     public static final String CLIENT_PROXY = "com.unrealdinnerbone.yaum.proxy.ClientProxy";
